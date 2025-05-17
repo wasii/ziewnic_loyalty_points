e5f479ec017c4aa27a3f10e253e292bbaef4230a
wasii <khatri.wasiq@gmail.com>
2025-05-17 15:20:10 +0500

    architecture initial setup

---
+++ android/app/src/main/kotlin/com/example/ziewnic_loyalty_points/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ziewnic_loyalty_points
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/components/constants.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: constant_identifier_names, prefer_interpolation_to_compose_strings
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColorString = '465263';
+const kPrimaryColor = Color(0xFF0066A8);
+const kSecondaryColor = Color.fromARGB(255, 0, 153, 255);
+const kTextColor = Color(0xFF12153D);
+const kTextLightColor = Color(0xFF9A9BB2);
+const kFillStarColor = Color(0xFFFCC419);
+const kErrorBackColor = Color(0xFFC72C41);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 0),
+  blurRadius: 10,
+  color: Colors.black26,
+);
+
+//URLs
+const BaseURL = "https://www.google.com/";
